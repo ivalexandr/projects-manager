1d92678a706e44ccd80d0283362ab273f6be6fd8
Alexandr Ivanin <ya@aivanin.ru>
2023-11-02 01:52:27 +0300

    feat(#PMP-2): added authentication

--- src/modules/auth/controllers/auth/auth.controller.ts
+++ src/modules/auth/controllers/auth/auth.controller.ts
@@ -3,13 +3,16 @@ import {
   Body,
   Controller,
   HttpStatus,
+  NotFoundException,
   Post,
   Res,
+  UnauthorizedException,
 } from '@nestjs/common';
 import { AuthService } from '../../services/auth/auth.service';
 import { CreateUserDto } from 'src/database/dto/create-user-dto';
 import { ResponseUserDto } from 'src/database/dto/response-user-dto';
 import { Response } from 'express';
+import { LoginUserDto } from 'src/database/dto/login-user-dto';
 
 @Controller('auth')
 export class AuthController {
@@ -29,6 +32,24 @@ export class AuthController {
     } catch (error) {
       if (error instanceof BadRequestException) {
         response.status(HttpStatus.BAD_REQUEST).send({ error: error.message });
+      }
+    }
+  }
+
+  @Post('login')
+  async login(@Body() loginUserDto: LoginUserDto, @Res() response: Response) {
+    try {
+      const user = await this.authService.login(loginUserDto);
+      const responseUser: ResponseUserDto = {
+        email: user.email,
+      };
+      response.status(HttpStatus.OK).send(responseUser);
+    } catch (error) {
+      if (error instanceof NotFoundException) {
+        response.status(HttpStatus.NOT_FOUND).send({ error: error.message });
+      }
+      if (error instanceof UnauthorizedException) {
+        response.status(HttpStatus.UNAUTHORIZED).send({ error: error.message });
       }
     }
   }
--- src/modules/auth/services/auth/auth.service.ts
+++ src/modules/auth/services/auth/auth.service.ts
@@ -1,7 +1,11 @@
-import { Injectable, BadRequestException } from '@nestjs/common';
+import {
+  Injectable,
+  BadRequestException,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { CreateUserDto } from 'src/database/dto/create-user-dto';
 import { UserService } from 'src/modules/user/services/user/user.service';
-import { CreateUser } from 'src/database/common/create-user';
+import { LoginUserDto } from 'src/database/dto/login-user-dto';
 import * as bcrypt from 'bcrypt';
 
 @Injectable()
@@ -22,13 +26,26 @@ export class AuthService {
 
     const salt = await bcrypt.genSalt(10);
 
-    const user: CreateUser = {
+    const user: CreateUserDto = {
       email: createUser.email,
       password: await bcrypt.hash(createUser.password, salt),
-      salt,
     };
 
     return await this.userService.create(user);
+  }
+
+  async login(loginUserDto: LoginUserDto) {
+    const user = await this.userService.getUser(loginUserDto.email);
+    const isValidPassword = await bcrypt.compare(
+      loginUserDto.password,
+      user.password,
+    );
+
+    if (!isValidPassword) {
+      throw new UnauthorizedException('Invalid password');
+    }
+
+    return user;
   }
 
   private passworValidate(password: string): boolean {
