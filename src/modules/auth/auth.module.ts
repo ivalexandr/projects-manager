46325ec07a43acc23de45973d5b596a4017222ac
Alexandr Ivanin <ya@aivanin.ru>
2023-11-03 00:52:16 +0300

    feat(#PMP-1,#PMP-2,#PMP-3): added jwt and refresh token

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -2,10 +2,24 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './controllers/auth/auth.controller';
 import { AuthService } from './services/auth/auth.service';
 import { UserModule } from '../user/user.module';
+import { JwtModule } from '@nestjs/jwt';
+import { JwtService } from './services/jwt/jwt.service';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    UserModule,
+    JwtModule.register({
+      global: true,
+      secret: process.env.JWT_SECRET,
+      signOptions: {
+        algorithm: 'HS256',
+        expiresIn: process.env.JWT_EXPIRES,
+        issuer: process.env.JWT_ISSUER,
+      },
+    }),
+  ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, JwtService],
+  exports: [JwtService],
 })
 export class AuthModule {}
