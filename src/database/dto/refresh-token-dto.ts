a202a3dc10570ad32589c4eef7bd00121fd0f36e
Alexandr Ivanin <ya@aivanin.ru>
2023-11-03 00:50:20 +0300

    feat(#PMP-1): added types

---
+++ src/database/dto/refresh-token-dto.ts
@@ -0,0 +1,4 @@
+export interface RefreshTokenDto {
+  email: string;
+  refresh_token: string;
+}
--- src/database/dto/response-user-dto.ts
+++ src/database/dto/response-user-dto.ts
@@ -1,3 +1,5 @@
 export interface ResponseUserDto {
   email: string;
+  access_token: string;
+  refresh_token: string;
 }
